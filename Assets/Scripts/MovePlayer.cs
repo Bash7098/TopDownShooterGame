996889899baf76f0c8c0d6c9cbf0c2ca923fec58
Bash7098 <145983831+Bash7098@users.noreply.github.com>
2023-11-06 12:52:22 +0000

    test1

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public MovementJoystick movementJoystick;
+    public float playerSpeed;
+    private Rigidbody2D rb;
+    private Vector2 movementDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (movementJoystick.joystickVec.y != 0)
+        {
+            movementDirection = new Vector2(movementJoystick.joystickVec.x, movementJoystick.joystickVec.y).normalized;
+            rb.velocity = movementDirection * playerSpeed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+        // Update the character's rotation based on the movement direction
+        if (movementDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
+    }
+}
