d63ec084a8d73058c6b70636094520f05f07e70d
Bash7098 <145983831+Bash7098@users.noreply.github.com>
2023-11-27 13:22:30 +0000

    handgun and shooting

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletForce = 10f;
+    public float fireRate = 0.5f;
+
+    private float nextFireTime = 0f;
+
+    void Update()
+    {
+        // Check for the spacebar key press and fire rate
+        if (Input.GetKey(KeyCode.Space) && Time.time >= nextFireTime)
+        {
+            Shoot();
+            nextFireTime = Time.time + 1f / fireRate; // Update the next fire time based on the fire rate
+        }
+    }
+
+    void Shoot()
+    {
+        // Instantiate a bullet at the fire point position and rotation
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        // Apply force to the bullet
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        }
+    }
+}
